fe54b9e6401268d897119fadfd3429e789f83f3b
JesusSavesC <150282499+JesusSavesC@users.noreply.github.com>
2023-12-04 16:17:57 -0700

    Add files via upload
    I did a thing

---
+++ PHP/Project/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="scripts/style.css">
+    <title>about me</title>
+    
+</head>
+<body>
+    <div class="top">
+        Thomas Labratories
+    </div>
+    <div class="sidebar">
+        info<br>info<br>more info
+    </div>
+    <span class="middle">
+        <h3>about me<br></h3>
+        <p>
+            <ul>
+                <li>4 years of HS</li>
+                <li>Started programming and never got good</li>
+            </ul>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec erat fermentum, malesuada elit non, porta nulla. Phasellus a viverra sapien, ut ullamcorper mauris. Vivamus id dui eget elit ultrices suscipit. Nunc fringilla dictum mollis. Vivamus dictum velit non varius luctus. Quisque hendrerit tortor vel diam egestas fringilla sit amet vitae elit. In hac habitasse platea dictumst. Morbi massa turpis, ullamcorper quis magna quis, suscipit sollicitudin leo. Nullam elementum, sapien a interdum luctus, sem felis eleifend urna, sed efficitur elit velit vel lacus. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed laoreet a neque ut pellentesque. Aliquam quam sapien, faucibus in enim ac, hendrerit ornare risus. Pellentesque in turpis turpis. Nulla facilisi. Donec volutpat varius feugiat.
+
+Vivamus dignissim augue non lacus tristique, ac tincidunt diam varius. Vestibulum bibendum convallis condimentum. Nulla tempus rhoncus fringilla. Sed rutrum molestie mollis. Phasellus convallis neque sit amet felis pellentesque elementum. Curabitur sodales dui et magna interdum interdum. Donec condimentum felis vel orci fermentum pellentesque. Sed dolor metus, aliquam ac lorem id, tempus mollis erat. Nulla at libero posuere, tempor mauris id, malesuada dui. Aenean fringilla mollis lacus, non tincidunt odio commodo ut. Fusce elementum viverra enim at auctor. Quisque luctus risus vitae dolor convallis pharetra.
+
+Cras metus urna, ornare nec gravida ac, placerat sed mauris. Pellentesque risus turpis, condimentum ut nunc et, lobortis viverra massa. Aliquam quis mollis metus. Mauris non orci purus. Quisque aliquam, libero sit amet congue feugiat, nulla orci porta urna, et venenatis orci tortor ac ipsum. Ut in porta mauris. Quisque dapibus accumsan risus nec vestibulum. In hac habitasse platea dictumst. Nulla imperdiet suscipit tortor sit amet elementum. Etiam vitae lobortis lacus.
+
+Aliquam volutpat, erat a luctus luctus, tortor sapien imperdiet dui, at efficitur mauris quam nec erat. Sed et fringilla nibh, vel facilisis nunc. Curabitur sodales eu enim eu feugiat. Ut ultrices nisl vitae nisl varius, vitae tempor nunc porttitor. In suscipit, libero sed varius scelerisque, ante leo ornare tortor, id interdum tellus sapien sed erat. Sed sollicitudin dui sit amet imperdiet sollicitudin. Praesent elementum ligula et hendrerit porta. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec pulvinar porta nisi, nec convallis lectus rhoncus quis. Mauris non magna faucibus, pharetra massa hendrerit, faucibus augue. Aenean sed massa sollicitudin, rutrum metus eget, pharetra ex.
+
+Maecenas ultricies sapien eget diam vestibulum bibendum. Sed lacus ex, volutpat non risus eu, convallis congue tellus. Integer molestie massa ligula, id sodales eros ullamcorper eget. Nullam quis leo tempus, lacinia nunc in, accumsan justo. Morbi mattis luctus rhoncus. Proin fringilla pulvinar sem, commodo lobortis sem facilisis nec. Suspendisse feugiat odio felis, sed rhoncus nunc suscipit sit amet. Donec auctor erat a orci interdum molestie.
+
+In quis consectetur quam. Praesent id ornare est. Duis elementum sem in nunc ultrices, id tempor turpis placerat. Donec tempus lacus ut pellentesque tincidunt. Praesent vel mauris dui. Suspendisse in mollis felis. Ut tempor accumsan tellus, nec pulvinar turpis accumsan eget.
+
+Nunc non eros sed ante scelerisque tincidunt sed eget enim. Mauris posuere ex quis ligula consectetur, non vestibulum sapien malesuada. Aliquam sit amet condimentum erat, sed malesuada nisl. Suspendisse potenti. Aenean at ullamcorper leo. Quisque placerat facilisis urna, at facilisis justo sagittis in. Praesent condimentum eu dolor sed lacinia. Aliquam dapibus est quis vehicula placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed aliquam augue felis, ut cursus dui consequat eget.
+
+Donec eu condimentum enim. Pellentesque quis consectetur ex. In convallis lacinia nulla, in imperdiet enim imperdiet et. Fusce at orci ipsum. Nam dapibus gravida tellus ultrices dapibus. Nunc ac convallis eros, eget egestas orci. Vivamus tristique auctor varius. Mauris auctor nulla id odio vulputate, et faucibus metus vulputate. Morbi ut convallis tellus. Etiam elementum accumsan suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce congue erat sed nulla laoreet dapibus. Pellentesque non fringilla nibh. Praesent iaculis non ipsum sed vestibulum. Etiam consequat magna vel egestas euismod. Donec finibus turpis vel leo elementum convallis.
+
+Aenean vitae nisl egestas, varius lectus a, mollis elit. Nullam lacinia dapibus bibendum. Cras iaculis cursus sapien, faucibus sagittis nulla. Integer nec ligula tincidunt, egestas nisl eu, tempus orci. Suspendisse potenti. Quisque elementum iaculis lectus nec facilisis. Nulla dignissim in est eget suscipit. Nunc sit amet tortor ac sem imperdiet dictum sit amet ut justo. Vivamus ac tellus eu sem pellentesque efficitur eu eget orci. Integer ullamcorper diam enim, condimentum mattis nisl varius nec. Curabitur lorem nunc, porttitor vitae sodales at, luctus in dolor. Integer urna nulla, dictum auctor congue ac, hendrerit et diam.
+
+Vestibulum a turpis risus. Pellentesque ornare orci in sagittis aliquet. Sed eu elit ultrices felis fermentum maximus. Donec porttitor mi mi, eget blandit purus mattis at. Quisque consequat euismod neque, sed sollicitudin ex mollis quis. Nullam id molestie felis. Aliquam eget euismod arcu. Proin a eros maximus, sagittis elit vel, laoreet felis. Etiam ornare erat non purus congue condimentum. Suspendisse potenti. Nullam id mi eu ante convallis vulputate.
+
+Integer eu varius mi, eleifend congue ipsum. Ut justo urna, convallis ac mi eu, semper condimentum urna. Duis molestie facilisis finibus. Nullam tempus lacinia nisi at cursus. Nam vehicula lacus sed lacinia elementum. Aenean ac dolor tincidunt, suscipit dui non, facilisis sapien. Suspendisse potenti. Suspendisse consectetur mi metus, ac scelerisque dui maximus et. Sed in facilisis arcu. Duis sollicitudin consequat ex a aliquam. Integer et pharetra urna. Aenean eu massa sed erat luctus condimentum. In ac dolor lacus. Proin sed elit sit amet magna tincidunt ultrices.
+
+Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc molestie, libero id feugiat ullamcorper, odio nunc dignissim leo, nec aliquet sem diam ut nulla. Donec pretium elit non felis efficitur faucibus. Nunc ultricies sit amet sem ullamcorper vulputate. Cras in mauris erat. Donec vitae justo tellus. Aliquam libero urna, laoreet a nunc at, interdum commodo lacus. Aenean blandit mi mauris, nec imperdiet libero rutrum sit amet. Proin vitae volutpat ante, eget eleifend nulla. Curabitur tristique, turpis pulvinar ultrices mattis, augue arcu rhoncus ex, id pretium risus nisl vitae nisl. Pellentesque eget sem hendrerit, ultrices nunc nec, interdum sem. Morbi aliquam, lectus vitae porttitor lobortis, sem lectus malesuada purus, et tincidunt elit erat non leo.
+
+Nunc accumsan porta cursus. Phasellus non dui sapien. Donec consectetur ligula sed risus molestie elementum. Donec tristique hendrerit dolor, ac feugiat neque molestie non. Cras posuere dolor id facilisis consectetur. Sed dictum orci purus, vitae dapibus neque gravida vitae. Pellentesque tempus consectetur sapien a ultricies. Ut scelerisque suscipit justo, sit amet molestie felis vestibulum aliquet.
+
+Morbi sed luctus nisi, a semper enim. Vivamus nec condimentum dui, vitae lacinia lectus. Donec non sapien mattis, consectetur nunc ac, imperdiet mauris. Quisque bibendum blandit enim, non laoreet arcu egestas ac. Sed quis enim mattis, scelerisque dolor et, semper mauris. Curabitur sit amet efficitur nisi. Nulla sed tincidunt magna. Etiam vulputate neque id odio mattis, ut imperdiet libero maximus. Maecenas est justo, molestie ac mattis et, tincidunt eget tortor. Suspendisse eget elementum sapien. In consequat vel tortor nec sagittis.
+
+Duis lobortis lacinia nibh, quis mollis dolor mollis quis. Nunc rutrum magna eros, et maximus nisl ultricies in. Integer sodales ligula lectus, ac auctor quam rutrum quis. Cras pretium placerat sem in pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque in sem ut velit interdum feugiat. Sed dignissim tincidunt mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta vestibulum massa eu feugiat. Nullam interdum nisl quis odio finibus mattis. Sed molestie, velit vitae consectetur bibendum, ipsum odio rhoncus velit, a dignissim magna mauris non tortor. Suspendisse id imperdiet nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+        </p>
+    </span>
+    </span>
+    <form action = "stuff.php" method = "get">
+        <table>
+        <tr>
+            First Name: <input type = "text" name = "nameF" autocomplete="John"><br>
+        </tr>
+        <tr>
+            Last Name: <input type = "text" name = "nameL" autocomplete="Cena"><br>
+        </tr>
+        <tr>
+            E-mail: <input type = "text" name = "email"><br>
+        </tr>
+        <input type = "submit">
+        </table>
+    </form>
+    <?php
+        echo 'Welcome to PHP <br/>';
+        echo date('Y-m-d H:i:s');
+    ?>
+</body>
